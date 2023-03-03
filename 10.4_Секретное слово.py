aa6c0d1c7f2ec2ee66e6892323ebc17a1918167d
olga <ryabinkova@hotmail.com>
2023-03-03 19:22:03 +0300

    Получилось !

--- 10.4_Секретное слово.py
+++ 10.4_Секретное слово.py
@@ -2,18 +2,21 @@ n = input()
 m = int(input())
 lst = []
 s1, s2, s3  = {}, {}, {}
+# создание словаря №1
 for i in n:
     s1[i] = s1.get(i,0) +1
+# создание словаря № 2
 for i in range(m):
     l = input().split(":")
     lst.append(l)
-
 s2 = {i[0]: i[1] for i in lst}
+# создание третьего словаря из первых двух
 for k, v in s1.items():
     for key, value in s2.items():
         value = int(value)
         if v == value:
             s3.setdefault(k, key)
+# расшифровка кодового слова через третий словарь
 result = []
 for i in n:
     for k,v in s3.items():
