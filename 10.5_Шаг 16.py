b68db9edcb88dd6e39a5fa46e74d09fbc9062d0f
olga <ryabinkova@hotmail.com>
2023-03-03 19:59:01 +0300

    Пока не решается

---
+++ 10.5_Шаг 16.py
@@ -0,0 +1,9 @@
+ids = ['S001', 'S002', 'S003', 'S004']
+names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle']
+grades = [86, 98, 89, 92]
+result= []
+s1 = {}
+for i, j in zip(names, grades):
+    s1.setdefault(i, j)
+
+result= []
